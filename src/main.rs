3bbb6ce182fccaa8ef22822704a8456befe88013
KonradKaralus <g78285423@gmail.com>
2024-08-27 19:21:34 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,221 @@
+use std::{collections::VecDeque, env};
+
+use operator::OperatorStore;
+
+mod operator;
+
+#[derive(Debug, PartialEq)]
+enum Token {
+    Number(f64),
+    Op(String),
+    Bracket(char),
+}
+
+fn l_bracket() -> Token {
+    Token::Bracket('(')
+}
+fn r_bracket() -> Token {
+    Token::Bracket(')')
+}
+
+fn round(x: f64, decimals: u32) -> f64 {
+    let y = 10i32.pow(decimals) as f64;
+    (x * y).round() / y
+}
+fn main() {
+    let mut os = OperatorStore::new();
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        panic!("no expression supplied");
+    }
+
+    let prec = match args.get(2) {
+        Some(s) => s.clone(),
+        None => "".to_string(),
+    };
+
+    let input = args.get(1).unwrap().clone();
+
+    let mut tokens = tokenizer(&input, &mut os);
+
+    tokens = process_negatives(tokens);
+
+    let mut res = calc(eval(tokens, &os), &os);
+
+    if prec != *"-p" {
+        res = round(res, 2);
+    }
+
+    println!("{}", res);
+}
+
+fn process_negatives(mut input: Vec<Token>) -> Vec<Token> {
+    if !input.contains(&Token::Op("-".into())) {
+        return input;
+    }
+
+    if *input.first().unwrap() == Token::Op("-".into()) {
+        input.insert(2, r_bracket());
+        input.insert(0, Token::Number(0f64));
+        input.insert(0, l_bracket());
+    }
+
+    let mut ins_pos: Vec<usize> = vec![];
+
+    for (pos, t) in input.iter().enumerate() {
+        if *t == Token::Bracket('(') && *input.get(pos + 1).unwrap() == Token::Op("-".into()) {
+            ins_pos.push(pos + 1);
+        }
+    }
+
+    ins_pos.reverse();
+
+    for pos in ins_pos {
+        input.insert(pos, Token::Number(0f64));
+    }
+
+    input
+}
+
+fn tokenizer(input: &str, operators: &mut OperatorStore) -> Vec<Token> {
+    let mut current = "".to_string();
+    let mut tokens = vec![];
+    let mut is_number = input.chars().next().unwrap().is_numeric();
+    let mut new = true;
+
+    for c in input.chars() {
+        if is_number && !(c.is_numeric() || c == '.') {
+            let number: f64 = current.parse().unwrap();
+            tokens.push(Token::Number(number));
+            new = true;
+        }
+
+        if new {
+            new = false;
+            current.clear();
+            is_number = c.is_numeric();
+            if ['(', ')'].contains(&c) {
+                tokens.push(Token::Bracket(c));
+                new = true;
+                continue;
+            }
+        }
+
+        if !is_number {
+            let m = operators.match_step(c);
+
+            if m.0 {
+                tokens.push(m.1.unwrap());
+                new = true;
+                continue;
+            }
+
+            current.push(c);
+        } else {
+            current.push(c);
+        }
+    }
+
+    if is_number {
+        let number: f64 = current.parse().unwrap();
+        tokens.push(Token::Number(number));
+    }
+
+    tokens
+}
+
+// Shunting-Yard-Algorithm
+fn eval(input: Vec<Token>, operators: &OperatorStore) -> VecDeque<Token> {
+    use {Token::Bracket, Token::Number, Token::Op};
+
+    let mut output_queue: VecDeque<Token> = VecDeque::new();
+    let mut operator_stack: Vec<String> = vec![];
+
+    for t in input {
+        match t {
+            Number(_) => output_queue.push_back(t),
+
+            Op(o) => {
+                if !operator_stack.is_empty() {
+                    loop {
+                        let last = operator_stack.last().unwrap();
+                        let last_precedence = operators.get_precedence(last);
+                        let precedence = operators.get_precedence(&o);
+
+                        if last_precedence >= precedence && last_precedence != 255 {
+                            output_queue.push_back(Op(operator_stack.pop().unwrap()));
+                        } else {
+                            break;
+                        }
+                        if operator_stack.is_empty() {
+                            break;
+                        }
+                    }
+                }
+                operator_stack.push(o);
+            }
+
+            Bracket(b) => {
+                if b == '(' {
+                    operator_stack.push("(".into());
+                } else if b == ')' && !operator_stack.is_empty() {
+                    loop {
+                        let last = operator_stack.last().unwrap();
+                        if last != "(" {
+                            output_queue.push_back(Token::Op(last.clone()));
+                            operator_stack.pop();
+                        } else {
+                            operator_stack.pop();
+                            break;
+                        }
+                        if operator_stack.is_empty() {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    loop {
+        let op = operator_stack.pop();
+        match op {
+            Some(o) => {
+                if o != "(" && o != ")" {
+                    output_queue.push_back(Op(o))
+                }
+            }
+            None => break,
+        }
+    }
+
+    output_queue
+}
+
+fn calc(mut input: VecDeque<Token>, operators: &OperatorStore) -> f64 {
+    use {Token::Number, Token::Op};
+
+    let mut stack: Vec<f64> = vec![];
+    while !input.is_empty() {
+        let token = input.pop_front().unwrap();
+        match token {
+            Number(i) => stack.push(i),
+            Op(o) => {
+                let calc = operators.calc_and_no_of_arguments(&o);
+                let mut operands = vec![];
+                for _ in 0..calc.1 {
+                    operands.push(stack.pop().unwrap());
+                }
+                operands.reverse();
+                let res = calc.0(operands);
+
+                stack.push(res);
+            }
+            _ => panic!("{:?} got into ended up in output stack", token),
+        }
+    }
+
+    stack[0]
+}
---
+++ src/operator.rs
@@ -0,0 +1,253 @@
+use std::{collections::HashMap, hash::Hash};
+
+use crate::Token;
+
+pub struct OperatorStore {
+    operators: HashMap<String, Box<dyn Operator>>,
+    matching: String,
+    current_matching: Vec<String>,
+}
+
+impl OperatorStore {
+    pub fn new() -> Self {
+        let mut new = Self {
+            operators: HashMap::new(),
+            matching: "".into(),
+            current_matching: vec![],
+        };
+
+        new.register_op(Box::new(Plus));
+        new.register_op(Box::new(Minus));
+        new.register_op(Box::new(Mul));
+        new.register_op(Box::new(Div));
+        new.register_op(Box::new(Pow));
+        new.register_op(Box::new(Sin));
+        new.register_op(Box::new(Tan));
+        new.register_op(Box::new(Cos));
+
+        new.new_match_step();
+
+        new
+    }
+
+    fn register_op(&mut self, operator: Box<dyn Operator>) {
+        self.operators.insert(operator.code(), operator);
+    }
+
+    pub fn new_match_step(&mut self) {
+        self.matching.clear();
+        for op in &self.operators {
+            self.current_matching.push(op.1.code());
+        }
+    }
+
+    pub fn match_step(&mut self, next: char) -> (bool, Option<Token>) {
+        self.matching.push(next);
+
+        self.current_matching
+            .retain(|code| code.starts_with(&self.matching));
+
+        if self.current_matching.len() == 1 {
+            let ret = Option::from(Token::Op(self.current_matching.remove(0)));
+            self.new_match_step();
+            return (true, ret);
+        }
+
+        (false, Option::None)
+    }
+
+    pub fn get_precedence(&self, code: &String) -> u8 {
+        if code == "(" || code == ")" {
+            return 255;
+        }
+
+        return self
+            .operators
+            .get(code)
+            .unwrap_or_else(|| panic!("unknown op"))
+            .precedence();
+    }
+
+    pub fn calc_and_no_of_arguments(&self, code: &String) -> (Calculation, u8) {
+        let op = self
+            .operators
+            .get(code)
+            .unwrap_or_else(|| panic!("unknown op"));
+
+        (op.calc(), op.no_of_operands())
+    }
+}
+
+pub type Calculation = Box<dyn Fn(Vec<f64>) -> f64>;
+
+pub trait Operator {
+    fn precedence(&self) -> u8;
+    fn code(&self) -> String;
+    fn calc(&self) -> Calculation;
+    fn no_of_operands(&self) -> u8;
+}
+
+impl Eq for dyn Operator {}
+impl Hash for dyn Operator {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.code().hash(state);
+        state.finish();
+    }
+}
+impl PartialEq for dyn Operator {
+    fn eq(&self, other: &Self) -> bool {
+        other.code() == self.code()
+    }
+}
+
+pub struct Plus;
+
+impl Operator for Plus {
+    fn code(&self) -> String {
+        "+".into()
+    }
+    fn precedence(&self) -> u8 {
+        1
+    }
+
+    fn calc(&self) -> Calculation {
+        Box::new(|x| x[0] + x[1])
+    }
+
+    fn no_of_operands(&self) -> u8 {
+        2
+    }
+}
+
+pub struct Minus;
+
+impl Operator for Minus {
+    fn code(&self) -> String {
+        "-".into()
+    }
+    fn precedence(&self) -> u8 {
+        1
+    }
+
+    fn calc(&self) -> Calculation {
+        Box::new(|x| x[0] - x[1])
+    }
+
+    fn no_of_operands(&self) -> u8 {
+        2
+    }
+}
+
+pub struct Mul;
+
+impl Operator for Mul {
+    fn code(&self) -> String {
+        "*".into()
+    }
+    fn precedence(&self) -> u8 {
+        2
+    }
+
+    fn calc(&self) -> Calculation {
+        Box::new(|x| x[0] * x[1])
+    }
+
+    fn no_of_operands(&self) -> u8 {
+        2
+    }
+}
+
+pub struct Div;
+
+impl Operator for Div {
+    fn code(&self) -> String {
+        "/".into()
+    }
+    fn precedence(&self) -> u8 {
+        2
+    }
+
+    fn calc(&self) -> Calculation {
+        Box::new(|x| x[0] / x[1])
+    }
+
+    fn no_of_operands(&self) -> u8 {
+        2
+    }
+}
+
+pub struct Pow;
+
+impl Operator for Pow {
+    fn code(&self) -> String {
+        "p".into()
+    }
+    fn precedence(&self) -> u8 {
+        3
+    }
+
+    fn calc(&self) -> Calculation {
+        Box::new(|x| x[0].powf(x[1]))
+    }
+
+    fn no_of_operands(&self) -> u8 {
+        2
+    }
+}
+
+pub struct Sin;
+
+impl Operator for Sin {
+    fn code(&self) -> String {
+        "sin".into()
+    }
+    fn precedence(&self) -> u8 {
+        4
+    }
+
+    fn calc(&self) -> Calculation {
+        Box::new(|x| x[0].sin())
+    }
+
+    fn no_of_operands(&self) -> u8 {
+        1
+    }
+}
+
+pub struct Cos;
+
+impl Operator for Cos {
+    fn code(&self) -> String {
+        "cos".into()
+    }
+    fn precedence(&self) -> u8 {
+        4
+    }
+
+    fn calc(&self) -> Calculation {
+        Box::new(|x: Vec<f64>| x[0].cos())
+    }
+
+    fn no_of_operands(&self) -> u8 {
+        1
+    }
+}
+
+pub struct Tan;
+
+impl Operator for Tan {
+    fn code(&self) -> String {
+        "tan".into()
+    }
+    fn precedence(&self) -> u8 {
+        4
+    }
+
+    fn calc(&self) -> Calculation {
+        Box::new(|x: Vec<f64>| x[0].tan())
+    }
+
+    fn no_of_operands(&self) -> u8 {
+        1
+    }
+}
